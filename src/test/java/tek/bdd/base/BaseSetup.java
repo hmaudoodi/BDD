a4287969c12f1a2d1b5b98bf7a4e3142ec26d051
hmaudoodi <maudoodiheala@gmail.com>
2024-07-30 18:31:05 -0400

    BDD commit

---
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -0,0 +1,42 @@
+package tek.bdd.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseSetup {
+
+    //Encapsulation driver instance
+    private static WebDriver driver;
+    public void setupBrowser() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(28));
+    }
+
+    /*
+    Web Driver insurance
+    Navigation to retail
+    maximize
+    and implicitly wait
+
+     */
+
+    public void quitBroswer() {
+        //null check before quit
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    //Give read only indirect access to driver
+    //restrict setting new driver istance
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+
+
+}
